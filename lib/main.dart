d24a9b5a6218f53b01545b7f76d69dbb266585a6
ddreambug <86515936+ddreambug@users.noreply.github.com>
2024-02-21 23:49:19 +0700

    editing color in one place (materialapp theming)

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,18 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 146, 226, 248));
 void main() {
   runApp(
-    const MaterialApp(home: Expenses()),
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: const Color.fromARGB(255, 79, 158, 180),
+            foregroundColor: kColorScheme.onPrimary),
+      ),
+      home: const Expenses(),
+    ),
   );
 }
