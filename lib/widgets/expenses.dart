f7e8c11776bc526264162643f36aed88aa71038d
ddreambug <86515936+ddreambug@users.noreply.github.com>
2024-02-21 23:21:52 +0700

    fallback message + undo for dismissible list

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -47,21 +47,37 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeItem(ExpenseModel expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
     setState(() {
       _registeredExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense Deleted.'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent =
+        const Center(child: Text('No expenses, try adding some'));
 
-    Widget mainContent = const Center(child: Text('No expenses, try adding some'));
-
-    if (_registeredExpenses.isNotEmpty){
+    if (_registeredExpenses.isNotEmpty) {
       mainContent = ExpensesList(
-            expenses: _registeredExpenses,
-            onRemoveExpense: _removeItem,
-          );
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeItem,
+      );
     }
 
     return Scaffold(
@@ -77,8 +93,7 @@ class _ExpensesState extends State<Expenses> {
       body: Column(
         children: [
           const Text('this will be chart'),
-          Expanded(
-              child: mainContent),
+          Expanded(child: mainContent),
         ],
       ),
     );
